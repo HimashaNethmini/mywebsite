41edf91f280e0c3e465a2f07aae65e619e2a496e
HimashaNethmini <himashanethmini05@gmail.com>
2023-12-21 22:30:57 +0530

    make the cursor

---
+++ src/components/cursor/Cursor.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import "./cursor.scss";
+
+const Cursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 }); {/* create use state and effect with event listener */}
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  return (
+    <motion.div
+      className="cursor"
+      animate={{ x: position.x+10, y: position.y+10 }} /*space between the cursor and mouse cursor */
+    ></motion.div>
+  );
+};
+
+export default Cursor;
