e69a8409fa471a3844b251fe5bf939116e321900
HimashaNethmini <himashanethmini05@gmail.com>
2023-12-21 22:20:24 +0530

    added all the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
+import "./app.scss";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 
 const App = () => {
   return (
     <div>
-      Hi
+      <Cursor /> {/*adding the cursor component */}
+      
+      {/* components linking to the website */}
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
     </div>
-  )
-}
+  );
+};
 
 export default App
 
