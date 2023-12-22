d66ccc7f10f8da206a91c54d00bbf3848505e86b
HimashaNethmini <himashanethmini05@gmail.com>
2023-12-22 21:15:56 +0530

    completed the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import './navbar.scss'
+import {motion} from "framer-motion";
+import Sidebar from '../sidebar/Sidebar';
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+
+      {/*sidebar */}
+      <Sidebar />
+      <div className="wrapper">
+        <motion.span initial={{opacity:0, scale:0.5}} 
+        animate={{opacity:1, scale:1}}
+        transition={{duration:0.5}}>
+          Hello
+       </motion.span>
+
+        <div className="social">
+
+            <a href='https://github.com/HimashaNethmini'><img src="/github.png" alt="" /></a>
+            {/*<a href='https://www.linkedin.com/in/himasha-nethmini-4b32a1231/'><img src="/linkedin.png" alt="" /></a>*/}
+            <a href='https://www.linkedin.com/in/himasha-nethmini-4b32a1231/'><img src="/1.png" alt="" /></a>
+            <a href='#'><img src="/youtube.png" alt="" /></a>
+            <a href='https://www.hackerrank.com/profile/himashanethmini1'><img src="/hackerrank.png" alt="" /></a>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
